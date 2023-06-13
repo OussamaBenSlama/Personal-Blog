bbbf675063c094cf209b8eee89d145a28e6c8b46
OussamaBenSlama <benslamaoussama1919@gmail.com>
2023-06-13 12:47:50 +0200

    add files

--- Personal-Blog-backend/Blog/settings.py
+++ Personal-Blog-backend/Blog/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myBlog',
         'USER': 'root',
-        'PASSWORD': 'issamfidaroudi1919@',
+        'PASSWORD': 'yourpassword',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
